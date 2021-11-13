5260262f8166973fa77377bd14d649816e8ac846
Oto Braz <oto.braz@outlook.com>
2021-11-13 10:31:56 -0300

    remove unused code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-const Discord = require('discord.js');
 const { token } = require('./config.json');
 
 const { Client, Intents } = require('discord.js');
@@ -8,25 +7,6 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({
    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-client.commands = new Discord.Collection();
-client.cooldowns = new Discord.Collection();
-
-// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const commandFolders = fs
-   .readdirSync('./commands')
-   .filter((file) => !file.endsWith('.js'));
-
-for (const folder of commandFolders) {
-   const commandFiles = fs
-      .readdirSync(`./commands/${folder}`)
-      .filter((file) => file.endsWith('.js'));
-   for (const file of commandFiles) {
-      // set a new item in the Collection
-      // with the key as the command name and the value as the exported module
-      const command = require(`./commands/${folder}/${file}`);
-      client.commands.set(command.name, command);
-   }
-}
 
 const eventFiles = fs
    .readdirSync('./events')
@@ -41,6 +21,4 @@ for (const file of eventFiles) {
    }
 }
 
-// login to Discord with your app's token
-// client.login(process.env.TOKEN);
 client.login(token);
